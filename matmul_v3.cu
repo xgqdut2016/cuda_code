e0b34a9abe79031d176e1c286ea1671eb164df13
xgqdut2016 <kenan_gewei@163.com>
2024-07-11 09:18:21 +0800

    one thread multi data

--- matmul_v3.cu
+++ matmul_v3.cu
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <cuda.h>
-const int TM = 8;
-const int TN = 8;
+const int TM = 4;
+const int TN = 4;
 const int BLOCK_DIM_x = 32;
 const int BLOCK_DIM_y = 32;
 const int BM = TM * BLOCK_DIM_x;
@@ -52,67 +52,6 @@ void compare(float *hostC, float *serialC, int M, int N)
 
 template <int BM, int BN, int BK, int TM, int TN>
 __global__ void matrixKernel1st(float *dA, float *dB, float *dC, int M, int K, int N)
-{
-    __shared__ float SA[BM][BK];
-    __shared__ float SB[BK][BN];
-    int indA = TM * (threadIdx.x + blockIdx.x * blockDim.x);
-    int indB = TN * (threadIdx.y + blockIdx.y * blockDim.y);
-    int width = (K + BK - 1) / BK;
-    float tmp[TM][TN] = {0.0f};
-    for (int ph = 0; ph < width; ph++)
-    {
-        for (int index_k = 0; index_k < BK; index_k++)
-        {
-            for (int index_q = 0; index_q < TM; index_q++)
-            {
-                if (indA + index_q < M && index_k + ph * BK < K)
-                {
-                    SA[threadIdx.x * TM + index_q][index_k] = dA[(indA + index_q) * K + index_k + ph * BK];
-                }
-                else
-                {
-                    SA[threadIdx.x * TM + index_q][index_k] = 0.0f;
-                }
-            }
-
-            for (int index_v = 0; index_v < TN; index_v++)
-            {
-                if (indB + index_v < N && index_k + ph * BK < K)
-                {
-                    SB[index_k][threadIdx.y * TN + index_v] = dB[(index_k + ph * BK) * N + indB + index_v];
-                }
-                else
-                {
-                    SB[index_k][threadIdx.y * TN + index_v] = 0.0f;
-                }
-            }
-        }
-        __syncthreads();
-        for (int index_q = 0; index_q < TM; index_q++)
-        {
-            for (int index_v = 0; index_v < TN; index_v++)
-            {
-                for (int index_k = 0; index_k < BK; index_k++)
-                {
-                    tmp[index_q][index_v] += SA[threadIdx.x * TM + index_q][index_k] * SB[index_k][threadIdx.y * TN + index_v];
-                }
-            }
-        }
-        __syncthreads();
-    }
-    for (int index_q = 0; index_q < TM; index_q++)
-    {
-        for (int index_v = 0; index_v < TN; index_v++)
-        {
-            if (indA + index_q < M && indB + index_v < N)
-            {
-                dC[(indA + index_q) * N + indB + index_v] = tmp[index_q][index_v];
-            }
-        }
-    }
-}
-template <int BM, int BN, int BK, int TM, int TN>
-__global__ void matrixKernel3rd(float *dA, float *dB, float *dC, int M, int K, int N)
 {
     __shared__ float SA[BM * BK];
     __shared__ float SB[BK * BN];
@@ -183,6 +122,69 @@ __global__ void matrixKernel3rd(float *dA, float *dB, float *dC, int M, int K, i
 template <int BM, int BN, int BK, int TM, int TN>
 __global__ void matrixKernel2nd(float *dA, float *dB, float *dC, int M, int K, int N)
 {
+    __shared__ float SA[BM * BK];
+    __shared__ float SB[BK * BN];
+    int indA = TM * (blockIdx.x * blockDim.x);
+    int indB = TN * (blockIdx.y * blockDim.y);
+    int width = (K + BK - 1) / BK;
+    float tmp[TM * TN] = {0.0f};
+    int tid = threadIdx.x + threadIdx.y * blockDim.x;
+    int smem_a_m = tid % 128;
+    int smem_a_k = tid / 128;
+    int smem_b_k = tid % 8;
+    int smem_b_n = tid / 8;
+    for (int ph = 0; ph < width; ph++)
+    {
+
+        if (indA + smem_a_m < M && smem_a_k + ph * BK < K)
+        {
+            SA[smem_a_m * BK + smem_a_k] = dA[(indA + smem_a_m) * K + smem_a_k + ph * BK];
+        }
+        else
+        {
+            SA[smem_a_m * BK + smem_a_k] = 0.0f;
+        }
+        if (indB + smem_b_n < N && smem_b_k + ph * BK < K)
+        {
+
+            SB[smem_b_k * BN + smem_b_n] = dB[(smem_b_k + ph * BK) * N + indB + smem_b_n];
+        }
+        else
+        {
+            SB[smem_b_k * BN + smem_b_n] = 0.0f;
+        }
+
+        __syncthreads();
+        for (int index_q = 0; index_q < TM; index_q++)
+        {
+            for (int index_v = 0; index_v < TN; index_v++)
+            {
+                int reg_c_m = threadIdx.x * TM + index_q;
+                int reg_c_n = threadIdx.y * TN + index_v;
+                for (int index_k = 0; index_k < BK; index_k++)
+                {
+                    tmp[index_q * TN + index_v] += SA[reg_c_m * BK + index_k] * SB[index_k * BN + reg_c_n];
+                }
+            }
+        }
+        __syncthreads();
+    }
+    for (int index_q = 0; index_q < TM; index_q++)
+    {
+        for (int index_v = 0; index_v < TN; index_v++)
+        {
+            int reg_c_m = threadIdx.x * TM + index_q;
+            int reg_c_n = threadIdx.y * TN + index_v;
+            if (indA + index_q < M && indB + index_v < N)
+            {
+                dC[(indA + reg_c_m) * N + indB + reg_c_n] = tmp[index_q * TN + index_v];
+            }
+        }
+    }
+}
+template <int BM, int BN, int BK, int TM, int TN>
+__global__ void matrixOrigin(float *dA, float *dB, float *dC, int M, int K, int N)
+{
 
     int indA = TM * (threadIdx.x + blockIdx.x * blockDim.x);
     int indB = TN * (threadIdx.y + blockIdx.y * blockDim.y);
@@ -235,8 +237,16 @@ void hostMatrix(float *hostA, float *hostB, float *hostC, int M, int K, int N)
     cudaEventCreate(&stop);
     cudaEventRecord(start, 0);
     // matrixKernel1st<BM, BN, BK, TM, TN><<<grid_dim, block_dim>>>(dA, dB, dC, M, K, N);
-    //  matrixKernel2nd<BM, BN, BK, TM, TN><<<grid_dim, block_dim>>>(dA, dB, dC, M, K, N);
-    matrixKernel3rd<BM, BN, BK, TM, TN><<<grid_dim, block_dim>>>(dA, dB, dC, M, K, N);
+    matrixKernel2nd<BM, BN, BK, TM, TN><<<grid_dim, block_dim>>>(dA, dB, dC, M, K, N);
+    //  matrixOrigin<BM, BN, BK, TM, TN><<<grid_dim, block_dim>>>(dA, dB, dC, M, K, N);
+
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess)
+    {
+        printf("CUDA Error: %s\n", cudaGetErrorString(err));
+        // Possibly: exit(-1) if program cannot continue....
+    }
+
     cudaEventRecord(stop, 0);
     cudaEventSynchronize(stop);
     cudaEventElapsedTime(&ker_time, start, stop); // must float ker_time
